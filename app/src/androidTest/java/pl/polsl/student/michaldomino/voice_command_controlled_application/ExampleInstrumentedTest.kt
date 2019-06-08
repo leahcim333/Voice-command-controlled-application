d2172fbc202f7bd91936bdb22c8a601d003fadb6
Michał Domino <leahcim333@o2.pl>
2019-06-08 12:51:57 +0200

    first commit

---
+++ app/src/androidTest/java/pl/polsl/student/michaldomino/voice_command_controlled_application/ExampleInstrumentedTest.kt
@@ -0,0 +1,24 @@
+package pl.polsl.student.michaldomino.voice_command_controlled_application
+
+import android.support.test.InstrumentationRegistry
+import android.support.test.runner.AndroidJUnit4
+
+import org.junit.Test
+import org.junit.runner.RunWith
+
+import org.junit.Assert.*
+
+/**
+ * Instrumented test, which will execute on an Android device.
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+@RunWith(AndroidJUnit4::class)
+class ExampleInstrumentedTest {
+    @Test
+    fun useAppContext() {
+        // Context of the app under test.
+        val appContext = InstrumentationRegistry.getTargetContext()
+        assertEquals("pl.polsl.student.michaldomino.voice_command_controlled_application", appContext.packageName)
+    }
+}
