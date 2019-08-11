892648fe4e09d2d58497f87bd37251bfe97b1480
Michał Domino <leahcim333@o2.pl>
2019-08-11 19:00:25 +0200

    Added entity.

--- app/src/main/java/pl/polsl/student/michaldomino/voice_command_controlled_application/data/database/model/Note.kt
+++ app/src/main/java/pl/polsl/student/michaldomino/voice_command_controlled_application/data/database/model/Note.kt
@@ -1,10 +1,35 @@
 package pl.polsl.student.michaldomino.voice_command_controlled_application.data.database.model
 
 import androidx.room.Entity
+import androidx.room.PrimaryKey
+import androidx.room.TypeConverter
+import pl.polsl.student.michaldomino.voice_command_controlled_application.data.view_model.note_selection.NoteType
 
 @Entity(tableName = "notes")
-class Note {
+data class Note(
 
-//    @PrimaryKey(autoGenerate = true)
-//    val noteId
+    @PrimaryKey(autoGenerate = true)
+    val noteId: Int,
+
+    val noteName: String,
+
+    val type: NoteType
+
+
+)
+
+class NoteTypeConverter {
+
+    companion object {
+        @TypeConverter
+        @JvmStatic
+        fun noteTypeToInt(noteType: NoteType): Int = noteType.typeId
+
+        @TypeConverter
+        @JvmStatic
+        fun intToNoteType(typeId: Int): NoteType {
+            return NoteType.values().find { it.typeId == typeId }!!
+        }
+    }
+
 }
