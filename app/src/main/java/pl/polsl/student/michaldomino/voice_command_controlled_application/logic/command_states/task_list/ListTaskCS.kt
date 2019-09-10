1eeae5b3da14d3dd107bcfc53fe2019b25fe6267
Michał Domino <leahcim333@o2.pl>
2019-09-10 10:53:32 +0200

    Renamed item to task.

--- app/src/main/java/pl/polsl/student/michaldomino/voice_command_controlled_application/logic/command_states/task_list/ListTaskCS.kt
+++ app/src/main/java/pl/polsl/student/michaldomino/voice_command_controlled_application/logic/command_states/task_list/ListTaskCS.kt
@@ -5,11 +5,11 @@ import pl.polsl.student.michaldomino.voice_command_controlled_application.logic.
 import pl.polsl.student.michaldomino.voice_command_controlled_application.ui.task_list.TaskListPresenter
 import pl.polsl.student.michaldomino.voice_command_controlled_application.view_model.task_list.TaskListItem
 
-class ListItemsCS(override val presenter: TaskListPresenter) : CSLeaf(presenter) {
+class ListTaskCS(override val presenter: TaskListPresenter) : CSLeaf(presenter) {
 
     private val ITEM_DELIMITER = ", "
 
-    override val commandNameId: Int? = R.string.list_items
+    override val commandNameId: Int? = R.string.list_tasks
 
     override fun initialize() {
         val itmes: MutableList<TaskListItem> = presenter.getItems()
--- app/src/main/java/pl/polsl/student/michaldomino/voice_command_controlled_application/logic/command_states/task_list/model/TaskListCommandStatesModel.kt
+++ app/src/main/java/pl/polsl/student/michaldomino/voice_command_controlled_application/logic/command_states/task_list/model/TaskListCommandStatesModel.kt
@@ -3,26 +3,26 @@ package pl.polsl.student.michaldomino.voice_command_controlled_application.logic
 import pl.polsl.student.michaldomino.voice_command_controlled_application.logic.command_states.base.AvailableCommandsCS
 import pl.polsl.student.michaldomino.voice_command_controlled_application.logic.command_states.base.BaseCommandState
 import pl.polsl.student.michaldomino.voice_command_controlled_application.logic.command_states.base.BaseCommandStateModel
-import pl.polsl.student.michaldomino.voice_command_controlled_application.logic.command_states.task_list.AddItemsCS
-import pl.polsl.student.michaldomino.voice_command_controlled_application.logic.command_states.task_list.EditItemSelectCS
-import pl.polsl.student.michaldomino.voice_command_controlled_application.logic.command_states.task_list.ListItemsCS
+import pl.polsl.student.michaldomino.voice_command_controlled_application.logic.command_states.task_list.AddTaskCS
+import pl.polsl.student.michaldomino.voice_command_controlled_application.logic.command_states.task_list.EditTaskSelectCS
+import pl.polsl.student.michaldomino.voice_command_controlled_application.logic.command_states.task_list.ListTaskCS
 import pl.polsl.student.michaldomino.voice_command_controlled_application.ui.task_list.TaskListPresenter
 
 class TaskListCommandStatesModel(override val presenter: TaskListPresenter) :
     BaseCommandStateModel(presenter) {
 
-    private val ADD_ITEMS = AddItemsCS(presenter)
+    private val ADD_TASKS = AddTaskCS(presenter)
 
     private val AVAILABLE_COMMANDS = AvailableCommandsCS(presenter, this)
 
-    private val EDIT_ITEM_SELECT = EditItemSelectCS(presenter)
+    private val EDIT_TASK_SELECT = EditTaskSelectCS(presenter)
 
-    private val LIST_ITEMS = ListItemsCS(presenter)
+    private val LIST_TASKS = ListTaskCS(presenter)
 
     override val availableCommandStates: List<BaseCommandState> = listOf(
-        ADD_ITEMS,
+        ADD_TASKS,
         AVAILABLE_COMMANDS,
-        EDIT_ITEM_SELECT,
-        LIST_ITEMS
+        EDIT_TASK_SELECT,
+        LIST_TASKS
     )
 }
