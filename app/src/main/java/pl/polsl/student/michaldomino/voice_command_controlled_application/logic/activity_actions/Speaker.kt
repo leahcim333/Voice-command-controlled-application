3fb64536ca75b81ac24c0370ca4641dc8b4559b1
Michał Domino <leahcim333@o2.pl>
2019-11-14 15:13:29 +0100

    Undo changes to Speaker.

--- app/src/main/java/pl/polsl/student/michaldomino/voice_command_controlled_application/logic/activity_actions/Speaker.kt
+++ app/src/main/java/pl/polsl/student/michaldomino/voice_command_controlled_application/logic/activity_actions/Speaker.kt
@@ -1,56 +1,27 @@
 package pl.polsl.student.michaldomino.voice_command_controlled_application.logic.activity_actions
 
+import android.content.Context
 import android.speech.tts.TextToSpeech
-import android.speech.tts.UtteranceProgressListener
-import pl.polsl.student.michaldomino.voice_command_controlled_application.ui.base.VoiceCommandsView
 import java.util.*
 
-
-class Speaker(private val view: VoiceCommandsView) {
+class Speaker(applicationContext: Context) {
 
     private lateinit var mTextToSpeech: TextToSpeech
 
     init {
-        mTextToSpeech =
-            TextToSpeech(view.getApplicationContext(), TextToSpeech.OnInitListener { status ->
+        mTextToSpeech = TextToSpeech(applicationContext, TextToSpeech.OnInitListener { status ->
             if (status != TextToSpeech.ERROR) {
                 //if there is no error then set language
                 mTextToSpeech.language = Locale.getDefault()
             }
         })
-        mTextToSpeech.setOnUtteranceProgressListener(object : UtteranceProgressListener() {
-            override fun onStart(utteranceId: String) {
-                val f = 0
-            }
-
-            override fun onDone(utteranceId: String) {
-                view.showToast("Finished")
-            }
-
-            override fun onError(utteranceId: String) {
-            }
-        })
     }
 
     fun speakInForeground(message: CharSequence?) {
-//        mTextToSpeech.setOnUtteranceProgressListener(object : UtteranceProgressListener() {
-//            override fun onStart(utteranceId: String) {
-//
-//            }
-//
-//            override fun onDone(utteranceId: String) {
-//                view.showToast("Finished")
-//            }
-//
-//            override fun onError(utteranceId: String) {
-//            }
-//        })
-//        mTextToSpeech.setOnUtteranceProgressListener()
-
         mTextToSpeech.speak(message, TextToSpeech.QUEUE_FLUSH, null, null)
-//        while (!mTextToSpeech.isSpeaking) {
-//        }
-//        while (mTextToSpeech.isSpeaking) {
-//        }
+        while (!mTextToSpeech.isSpeaking) {
+        }
+        while (mTextToSpeech.isSpeaking) {
+        }
     }
 }
